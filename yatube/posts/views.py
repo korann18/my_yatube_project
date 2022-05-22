b6263f6a05ea654060981e238b658ecba250889d
korann18 <60140258+korann18@users.noreply.github.com>
2022-05-22 22:28:25 +0200

    commit 1.0
    Задание выполнено

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Group, Post
 
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')
+def group_list(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+

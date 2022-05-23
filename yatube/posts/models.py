1506d2851a53e60b3a299a1830f031796d5e7f2c
korann18 <60140258+korann18@users.noreply.github.com>
2022-05-23 09:57:41 +0200

    commit 3.0

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,34 +5,37 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts')
+        related_name='posts',
+        verbose_name='Автор'
+    )
     group = models.ForeignKey(
         "Group",
         related_name='posts',
         on_delete=models.SET_NULL,
         blank=True,
-        null=True
+        null=True,
+        verbose_name='Сообщество'
     )
 
     class Meta:
         verbose_name = 'Публикация'
         verbose_name_plural = 'Публикации'
-        ordering = ["-pub_date"]
+        ordering = ("-pub_date", )
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, )
-    description = models.TextField()
+    title = models.CharField('Название', max_length=200)
+    slug = models.SlugField('Ссылка на группу', unique=True)
+    description = models.TextField('Описание')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
+        verbose_name = 'Сообщество'
+        verbose_name_plural = 'Сообщества'
